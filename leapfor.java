7840b4545b4c518c3867c3b27be132c80123ab3a
TejashriMuthyala <123165311+TejashriMuthyala@users.noreply.github.com>
2023-02-09 20:28:05 +0530

    09-02-23 - java practice

---
+++ leapfor.java
@@ -0,0 +1,13 @@
+class table{
+    public static void main(String args[]){
+        int i;
+        for(i=1900;i<=2023;i++)
+        {
+            if(i%4==0)
+            {
+                System.out.println(i+" is a leap year");
+            }
+
+        }
+    }
+}
