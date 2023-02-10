1ea658624a9e5d014dee04a2901ed64a420a37d7
TejashriMuthyala <123165311+TejashriMuthyala@users.noreply.github.com>
2023-02-10 20:09:38 +0530

    10-02-23 java practise

---
+++ palindrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class palindrome{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num=sc.nextInt();
+        int rem,sum=0; int temp=num;
+        while(num!=0)
+        {
+            rem=num%10;
+            sum=sum*10+rem;
+            num=num/10;
+        }
+        if(sum==temp)
+        {
+            System.out.println(temp+" is a palindrome");
+        }
+        else
+        {
+            System.out.println(temp+" is not a palindrome");
+        }
+    }
+}
