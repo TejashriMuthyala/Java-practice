1ae26eb7be8c5e9dbffa47f9d6eee690d169e8d6
TejashriMuthyala <123165311+TejashriMuthyala@users.noreply.github.com>
2023-02-16 19:16:48 +0530

    15-02-23 java practise

--- cal.java
+++ cal.java
@@ -7,16 +7,23 @@ class cal{
         int num1=sc.nextInt(); //taking first parameter from user
         System.out.println("Enter another value");
         int num2=sc.nextInt(); //taking second parameter from user
+        System.out.println("Enter your choice: 1 for Addition, 2 for Subtraction, 3 for Multiplication, 4 for Division");
+        int choice=sc.nextInt(); //taking the choice of the user
         cal ops=new cal();
         int addition=ops.add(num1,num2); //method call with parameters
         int subtraction=ops.sub(num1,num2); //method call with parameters
         int multiplication=ops.mul(num1,num2); //method call with parameters
         int division=ops.div(num1,num2);//method call with parameters
-        System.out.println("The addition of "+num1+" and "+num2+" is: "+addition); //printing addition
-        System.out.println("The subtraction of "+num1+" and "+num2+" is: "+subtraction); //printing subtraction
-        System.out.println("The multiplication of "+num1+" and "+num2+" is: "+multiplication); //printing multiplication
-        System.out.println("The division of "+num1+" and "+num2+" is: "+division); //printing multiplication
-
+        System.out.println(
+        switch(choice)
+        {
+            case 1->"The addition of "+num1+" and "+num2+" is: "+addition; //printing addition
+            case 2->"The subtraction of "+num1+" and "+num2+" is: "+subtraction; //printing subtraction
+            case 3->"The multiplication of "+num1+" and "+num2+" is: "+multiplication; //printing multiplication
+            case 4->"The division of "+num1+" and "+num2+" is: "+division; //printing division
+            default->"Invalid choice!";
+        }
+        );
     }
     public int add(int num1,int num2){
         return num1+num2; //addition operation
@@ -25,9 +32,9 @@ class cal{
         return num1-num2; //subtraction
     }
     public int mul(int num1,int num2){
-        return num1*num2; //subtraction
+        return num1*num2; //multiplication
     }
     public int div(int num1,int num2){
-        return num1/num2; //subtraction
+        return num1/num2; //division
     }
 }
