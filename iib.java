cfe5c3671449af6c14ddc1440363ff51bf8563d0
TejashriMuthyala <123165311+TejashriMuthyala@users.noreply.github.com>
2023-03-01 19:44:16 +0530

    01-03-23 java practise

---
+++ iib.java
@@ -0,0 +1,13 @@
+class IIB{
+    public static void main(String args[]){
+        IIB ib=new IIB();
+        System.out.println("Hi from main");
+    }
+    IIB(){
+        System.out.println("Hi from constructor");
+    }
+    {
+        System.out.println("Hi from IIB");
+
+    }
+}
